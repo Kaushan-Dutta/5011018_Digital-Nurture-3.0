df8590a90e7b1f54e1d4801d4dcc2f51f34ea716
Kaushan Dutta <94303484+Kaushan-Dutta@users.noreply.github.com>
2024-07-29 01:11:22 +0530

    Delete Practices directory

--- Practices/Factory_Design/os_package/Android.java
+++
@@ -1,9 +0,0 @@
-package Practices.Factory_Design.os_package;
-
-public class Android implements OS{
-    @Override
-    public void spec() {
-        System.out.println("Most Powerful OS");
-    }
-
-}
--- Practices/Factory_Design/os_package/OS.java
+++
@@ -1,5 +0,0 @@
-package Practices.Factory_Design.os_package;
-
-public interface OS{
-    void spec();
-}
